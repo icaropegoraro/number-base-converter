1f0a30fda540d47c9efcf1e8fd207e014fc0f912
icaropegoraro <icaropegsakurai@gmail.com>
2025-08-15 12:33:34 -0300

    Nao lembro oq eu fiz

--- services/index.js
+++ services/index.js
@@ -1,36 +1,36 @@
 import { hasForbiddenDigits, isString, isEmpty } from './utils/index.js'
 
-export const convert = ((number, base, newBase) => {
-     const digitChars  = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-     let decimalNumber = 0
+export const convert = (number, base, newBase) => {
+     const chars  = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+     let numberInDecimal = 0
 
-     let convertedNumber = ''
+     let resultNumber = ''
+
+     if (!isString(number) || !isString(base) || !isString(newBase)) {
+          return console.log('Insira o número e a base em formato de string.')
+     }
      
      if (isEmpty([number, base, newBase])) {
           return console.log('Insira um número que não seja vazio')
      }
      
-     if (hasForbiddenDigits(digitChars, number, base)) {
+     if (hasForbiddenDigits(chars, number, base)) {
           return console.log('Essa base não aceita algum caractere de seu número')
-     }
-
-     if (!isString(number) || !isString(base) || !isString(newBase)) {
-          return console.log('Insira o número em formato de string (ex: convertOtherToDecimal("1100", "2").')
      }
 
      number = number.split('').reverse().join('')
      base = parseInt(base)
+     newBase = parseInt(newBase)
 
-     for (let charIndex = 0; charIndex <= (number.length - 1); charIndex++) {
-          let numberToAdd = parseInt(number[charIndex]) * base ** charIndex
-          decimalNumber += numberToAdd
+     for (let numberPosition = 0; numberPosition <= (number.length - 1); numberPosition++) {
+          let digit = number[numberPosition]
+          let numberToSum = parseInt(digit) * base ** numberPosition
+          numberInDecimal += numberToSum
      }
 
-     number = decimalNumber
+     let absoluteValue = Math.abs(numberInDecimal)
 
-     let absoluteValue = Math.abs(number)
-
-     if (isNaN(number) || isNaN(newBase)) {
+     if (isNaN(numberInDecimal) || isNaN(newBase)) {
           return console.log("Insira um número válido no número ou nas bases")
      }
 
@@ -38,21 +38,21 @@ export const convert = ((number, base, newBase) => {
      const isZero = number === 0
 
      if (isZero) {
-          convertedNumber = '0'
-          console.log(convertedNumber)
-          return convertedNumber
+          resultNumber = '0'
+          console.log(resultNumber)
+          return resultNumber
      }
 
      while (absoluteValue > 0) {
           let remainder = (absoluteValue % newBase)
           absoluteValue = Math.floor(absoluteValue / newBase)
-          convertedNumber = digitChars[remainder] + convertedNumber
+          resultNumber = chars[remainder] + resultNumber
      }
 
      if (isNegative) {
-          convertedNumber = '-' + convertedNumber
+          resultNumber = '-' + resultNumber
      }
 
-     console.log(convertedNumber)
+     console.log(resultNumber)
 
-})("1100", "", "10")
+}
